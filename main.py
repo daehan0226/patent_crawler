ec8121365283f546cea7e28ea8b9d225984eeff4
daehan <dh16931@gmail.com>
2022-01-03 08:35:08 +0900

    feat: download loop process

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from Crawler import Crawler
 from config.config import config
 
@@ -23,7 +24,11 @@ def run_crawler():
     crawler.send_text_to_input(elements["ad_to_input"], config["search_data_to"])
     crawler.click_button(elements["search_button"])
 
-    # Download
+    # Total patent count
+    patent_count_str = crawler.get_text(elements["total_count"])
+    patent_count = int(patent_count_str.replace(",", ""))
+
+    # Download setup
     crawler.click_button(elements["download_popup_button"])
     crawler.switch_tab(1)
     crawler.select_options(
@@ -31,6 +36,23 @@ def run_crawler():
         config["download_data_list"],
         elements["select_move_button"],
     )
+
+    # Download loop
+    download_index_from = 1
+    download_count_at_once = config["download_count_at_once"]
+    while download_index_from < int(patent_count):
+        crawler.send_text_to_input(
+            elements["download_index_from_input"], download_index_from
+        )
+        crawler.send_text_to_input(
+            elements["download_index_to_input"],
+            download_index_from + (download_count_at_once - 1),
+        )
+
+        crawler.click_button(elements["create_file_button"])
+        crawler.wait_for_alert(config["create_file_wait"])
+        crawler.click_button(elements["download_button"])
+        download_index_from += download_count_at_once
 
     crawler.quit()
 
